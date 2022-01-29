5135afbc13c874cd16d22f92cdf8fe1d79388f7d
AnnaKucherenko <annakucherenko31@gmail.com>
2022-01-29 14:49:25 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+const onPlayerTime = function(dataTime) {
+    const dataTimeJSON = JSON.stringify(dataTime);
+    localStorage.setItem(STORAGE_KEY, dataTimeJSON);
+    
+};
+player.on('timeupdate', throttle(onPlayerTime, 1000));
+
+const savedDataTime = localStorage.getItem(STORAGE_KEY);
+const objectDataTime = JSON.parse(savedDataTime);
+
+player.setCurrentTime(objectDataTime.seconds).then(function(seconds) {
+    seconds = objectDataTime.seconds
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+
+        default:
+            break;
+    }
+});
+
 
